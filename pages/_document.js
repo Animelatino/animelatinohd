27264f2accedd5c4fe0cfbf289d65b2f7738b4c3
Dark <malcarichard6@gmail.com>
2020-12-24 01:34:12 -0500

    add manifest

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="icon" href="/images/favicon.ico"/>
                     <script async src="https://arc.io/widget.min.js#R2yjvhvV"></script>
+                    <link rel="manifest" href="/manifest.json"/>
                 </Head>
                 <body>
                     <Main />
