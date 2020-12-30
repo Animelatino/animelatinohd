c7d01b68a365e7fd0d688cf6937ae21fa22696da
Dark <malcarichard6@gmail.com>
2020-12-30 02:16:17 -0500

    chat.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,7 @@ class MyDocument extends Document {
                     <script async src="https://arc.io/widget.min.js#R2yjvhvV"></script>
                     <link rel="manifest" href="/manifest.json"/>
                     <meta name="theme-color" content="#000000"/>
-                    <script id="chatBroEmbedCode">
-                        function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
-                        ChatbroLoader({encodedChatId: '67LDD'});
-                    </script>
+                    <script id="chatBroEmbedCode" src="/chat.js"></script>
                 </Head>
                 <body>
                     <Main />
