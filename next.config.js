832773449f25a0b8b21005bbc8b301004396abc3
Dark <malcarichard6@gmail.com>
2021-02-05 14:36:08 -0500

    private ENV

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const withImages = require('next-images')
 
 const nextConfig = {
     images: {
-        domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org']
     }
 }
 
