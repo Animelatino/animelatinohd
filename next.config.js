e606ea19dba5d721f1fdb917486ff120882565f0
Dark <malcarichard6@gmail.com>
2021-02-05 14:39:06 -0500

    fixed ENV

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const withImages = require('next-images')
 
 const nextConfig = {
     images: {
-        domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org']
+        domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    },
+    env: {
+        NAME: 'AnimeLHD',
+        URL: 'https://www.animelatinohd.com/',
+        APIURL: 'https://api.animelatinohd.com/api/',
+        STREAMURL: 'https://api.animelatinohd.com/stream/',
+        GA_TRACKING_ID: 'UA-162278791-1',
+        DISQUS_SHORTNAME: 'animelatinohd'
     }
 }
 
