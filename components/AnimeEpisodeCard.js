c6bd09ef58989a12145243b9de3a114a25c52c83
Dark <malcarichard6@gmail.com>
2021-02-08 00:36:01 -0500

    add markView Episode

--- components/AnimeEpisodeCard.js
+++ components/AnimeEpisodeCard.js
@@ -35,7 +35,12 @@ export default class AnimeEpisodeCard extends Component {
                     <Link href={slugEpisode(anime?.slug, episode?.number)}>
                         <a className={styles.title} alt={`${anime?.name} ${episode?.number}`} title={`${anime?.name} ${episode?.number}`}>
                             <div className={styles.limit}>{anime?.name}</div>
-                            <span className={styles.episode}>{`Ep. ${episode?.number}`}</span>
+                            <span className={styles.episode}>
+                                <span className={styles.checkView}>
+                                    <svg viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M12 5C5 5 2 11 2 12s3 7 10 7 10-6 10-7-3-7-10-7zm0 12c-4 0-7-4-8-5 1-1 4-5 8-5s7 4 8 5c-1 1-4 5-8 5z"></path><path d="M12 8a4 4 0 104 4 4 4 0 00-4-4zm0 6a2 2 0 112-2 2 2 0 01-2 2z"></path></g></svg>
+                                </span>
+                                {`Ep. ${episode?.number}`}
+                            </span>
                         </a>
                     </Link>
                 </div>
