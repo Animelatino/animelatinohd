102b12583ae01dd52633f93c73dade60fd3c796f
Dark <malcarichard6@gmail.com>
2021-02-28 17:27:11 -0500

    replace Next PWA to SW

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-const withPlugins = require('next-compose-plugins')
-const withImages = require('next-images')
+const withPWA = require('next-pwa')
 
 const nextConfig = {
+    mode: 'production',
+    pwa: {
+        dest: 'public',
+        register: true,
+        scope: '/',
+        sw: 'service-worker.js',
+        importScripts: ["https://arc.io/arc-sw-core.js"],
+        offlineGoogleAnalytics: false
+    },
     images: {
         domains: ['i.imgur.com','*.tmdb.org','www.themoviedb.org'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -9,7 +17,7 @@ const nextConfig = {
     },
     env: {
         NAME: 'AnimeLHD',
-        URL: 'https://www.animelatinohd.com',
+        URL: 'http://www.animelatinohd.com',
         APIURL: 'https://api.animelatinohd.com/api/',
         STREAMURL: 'https://api.animelatinohd.com/stream/',
         GA_TRACKING_ID: 'UA-162278791-1',
@@ -17,4 +25,4 @@ const nextConfig = {
     }
 }
 
-module.exports = withPlugins([[withImages]], nextConfig)
+module.exports = withPWA(nextConfig)
--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,8 @@ class MyDocument extends Document {
             <Html lang="es">
                 <Head>
                     <link rel="icon" href="/favicon.ico"/>
-                    <link rel="manifest" href="/manifest.json"/>
                     <meta name="theme-color" content="#000000"/>
+                    <link rel="manifest" href="/manifest.json"/>
                     <meta name="robots" content="index, follow"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"/>
