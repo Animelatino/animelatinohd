f3759b7545482a5a9ddcd82fdfaeea5ab9c67658
AnimeLatino <malcarichard6@gmail.com>
2021-07-09 18:11:46 -0500

    Update AdsScript.js

--- components/AdsScript.js
+++ components/AdsScript.js
@@ -4,21 +4,24 @@ import styles from '../styles/Ads.module.css';
 
 class AdsScript extends PureComponent {
 
-    componentDidMount () {
-        //const script = document.createElement("script");
-        //script.type = "application/javascript";
-        //script.setAttribute("data-idzone", "4236022");
-        //script.src = "https://a.exdynsrv.com/nativeads-v2.js";
-        //script.async = true;
-        //const div = document.getElementById('ads_inline');
-        //div.appendChild(script);
-    }
+    // componentDidMount () {
+    //     const script = document.createElement("script");
+    //     script.type = "application/javascript";
+    //     script.setAttribute("data-idzone", "4236022");
+    //     script.src = "https://a.exdynsrv.com/nativeads-v2.js";
+    //     script.async = true;
+    //     const div = document.getElementById('ads_inline');
+    //     div.appendChild(script);
+    // }
     
+    // render() {
+    //    return (
+    //        <div id={'ads_inline'} className={`${this.props.className} ${styles.container}`}></div>
+    //    )
+    // }
+
     render() {
-       // return (
-       //     <div id={'ads_inline'} className={`${this.props.className} ${styles.container}`}></div>
-       // )
-       return null
+        return null
     }
 }
 
